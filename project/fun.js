efee66b36c2dd0b06d0347ebf62fe4df2d6b30ca
pyatasenyuk <36247802+pyatasenyuk@users.noreply.github.com>
2018-03-26 18:22:37 +0300

    final

--- project/fun.js
+++ project/fun.js
@@ -1,10 +1,11 @@
  $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+    $("[name='menu']").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
+        $('body,html').animate({scrollTop: top}, 'slow');
     });
 });
 
-
+     
+    
