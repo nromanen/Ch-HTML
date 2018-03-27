deff5cc277949401fb190f5c9a3279ffa752d7ef
Tatiana1103 <tasia1033@gmail.com>
2018-03-27 19:44:47 +0300

    third adittion

--- project/js/script.js
+++ project/js/script.js
@@ -1,4 +1,4 @@
-icon.onclick = function() {
+function clicker() {
     var x = document.getElementById("myTopnav");
 
     if(x.className === "topnav") {
@@ -37,27 +37,41 @@ $(document).ready(function(){
     /* Ajax-запрос */
 
  
+    $('#btn-send').click(function() {
+        // Please set your email in url field
+        $.ajax({
+            url: "https://formspree.io/YOUR_EMAIL@HERE",  
+            method: "POST",
+            data: {
+                   email: email.value,
+                   message: comment.value
+                   },
+            dataType: "json"
+        }).done(function() {
+           $('#container').html('<h1>Thank you!</h1>')
+        } );
+      });
 
-FUNCTION SendPost() // Наша функция, которая будет осуществлять ajax-отправку
+//FUNCTION SendPost() // Наша функция, которая будет осуществлять ajax-отправку
 
-{
+//{
 
-jQuery.ajax({    // Начала конструкции для работы с Ajax через jQuery
+//jQuery.ajax({    // Начала конструкции для работы с Ajax через jQuery
 
-type: "GET", // Метод, которым получаем данные из формы
+//type: "GET", // Метод, которым получаем данные из формы
 
-url: "/send.php", // Обработчик формы.
+//url: "/send.php", // Обработчик формы.
 
-data: jQuery("#form").serialize(), // Этот метод, берет форму с id=form и достает оттуда данные
+//data: jQuery("#form").serialize(), // Этот метод, берет форму с id=form и достает оттуда данные
 
-success: FUNCTION(html) {    // функция выполняемая при успешном отправлении данных
+//success: FUNCTION(html) {    // функция выполняемая при успешном отправлении данных
 
-jQuery("body").empty();    // очищаем тело документа
+//jQuery("body").empty();    // очищаем тело документа
 
-jQuery("body").append("<h2><center>Ваше сообщение успешно отправлено.</center></h2>"); // вставляем сообщение об успехе
+//jQuery("body").append("<h2><center>Ваше сообщение успешно отправлено.</center></h2>"); // вставляем сообщение об успехе
 
-}
+//}
 
-});
+//});
 
-}
+//}
